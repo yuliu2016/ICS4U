ef11587c4db3ca0261196978b42599cfbd6261b9
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-04-10 10:45:04 -0400

    q3

--- src/main/java/ics4u/a7/A7P274Q3.java
+++ src/main/java/ics4u/a7/A7P274Q3.java
@@ -1,12 +1,16 @@
 package ics4u.a7;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+
 /**
  * Assignment 6
  * Question 5
- *
+ * <p>
  * Write a deﬁnition of an equals method for the Fraction class. Your method should
  * return true if and only if the Fraction objects being compared represent equivalent fractions.
- *
+ * <p>
  * ICS4U class 2019
  *
  * @author Yu Liu
@@ -15,13 +19,30 @@ package ics4u.a7;
 public class A7P274Q3 {
 
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(10, 2);
-        Fraction f2 = new Fraction(15, 3);
-        if (f1.equals(f2)) {
-            System.out.println("F1 equals F2");
-        } else {
-            System.out.println("F1 does not equal F2");
+        Fraction a = getUserInputFraction("a");
+        Fraction b = getUserInputFraction("b");
+        Fraction c = getUserInputFraction("c");
+        // calculate the result of a * n - c^2
+        Fraction result = a.times(b).plus(c.times(c).negate()).reduced();
+        System.out.println("a * b - c^2 = " + result);
+        // calculate sum of squares of numerator and denominator
+        System.out.println("sum of squares = " + result.num * result.num + result.den * result.den);
+    }
+
+    /**
+     * Get the user input of a Fraction
+     */
+    private static Fraction getUserInputFraction(String name) {
+        System.out.print("Enter the numerator for fraction " + name + ": ");
+        int num = _In.getInt();
+        System.out.print("Enter the denominator for fraction " + name + ": ");
+        int den = _In.getInt();
+        while (den == 0) {
+            System.out.println("The denominator cannot be zero");
+            System.out.print("Enter the denominator for fraction " + name + ": ");
+            den = _In.getInt();
         }
+        return new Fraction(num, den);
     }
 
     /**
@@ -39,11 +60,23 @@ public class A7P274Q3 {
         }
 
         /**
+         * Check if the Fraction is equal to another in value
+         */
+        public boolean equals(Object obj) {
+            if (obj instanceof Fraction) {
+                Fraction other = (Fraction) obj;
+                return num / den == other.num / other.den;
+            }
+            return false;
+        }
+
+
+        /**
          * Reduces the fraction to lowest terms
          */
         Fraction reduced() {
             int gcd = gcd(num, den);
-            return new Fraction(num / gcd, den / gcd);
+            return gcd > 1 ? new Fraction(num / gcd, den / gcd) : new Fraction(num, den);
         }
 
         /**
@@ -54,14 +87,24 @@ public class A7P274Q3 {
         }
 
         /**
-         * Check if the Fraction is equal to another in value
+         * Returns the sum of this fraction and another fraction
          */
-        public boolean equals(Object obj) {
-            if (obj instanceof Fraction) {
-                Fraction other = (Fraction) obj;
-                return num/den == other.num/other.den;
-            }
-            return false;
+        Fraction plus(Fraction other) {
+            return new Fraction(num * other.den + other.num * den, den * other.den);
+        }
+
+        /**
+         * Returns the inverse (reciprocal) of this fraction
+         */
+        private Fraction negate() {
+            return new Fraction(-num, den);
+        }
+
+        /**
+         * Returns the product of this fraction and another fraction
+         */
+        private Fraction times(Fraction other) {
+            return new Fraction(num * other.num, den * other.den);
         }
 
         /**
@@ -69,6 +112,63 @@ public class A7P274Q3 {
          */
         public String toString() {
             return "Fraction(num = " + num + ", den = " + den + ")";
+        }
+    }
+
+
+    private static class _In {
+        private static InputStreamReader r = new InputStreamReader(System.in);
+        private static BufferedReader br = new BufferedReader(r);
+
+        // Read a String from standard system input
+        static String getString() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                return "";
+            }
+        }
+
+        // Read a Number as a String from standard system input
+        // Return the Number
+        private static Number getNumber() {
+            String numberString = getString();
+            try {
+                numberString = numberString.trim().toUpperCase();
+                return NumberFormat.getInstance().parse(numberString);
+            } catch (Exception e) {
+                // If any exception occurs, just return zero
+                return new Integer(0);
+            }
+        }
+
+        // Read an int from standard system input
+        static int getInt() {
+            return getNumber().intValue();
+        }
+
+        // Read a long from standard system input
+        static long getLong() {
+            return getNumber().longValue();
+        }
+
+        // Read a float from standard system input
+        static float getFloat() {
+            return getNumber().floatValue();
+        }
+
+        // Read a double from standard system input
+        static double getDouble() {
+            return getNumber().doubleValue();
+        }
+
+        // Read a char from standard system input
+        static char getChar() {
+            String s = getString();
+            if (s.length() >= 1)
+                return s.charAt(0);
+            else
+                return '\n';
         }
     }
 }
