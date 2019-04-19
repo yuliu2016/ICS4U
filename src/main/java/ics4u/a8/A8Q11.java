da0d820363cc1b8954b8d4f6718fe49f80ba03a3
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-04-19 13:44:31 -0400

    sieve

--- src/main/java/ics4u/a8/A8Q11.java
+++ src/main/java/ics4u/a8/A8Q11.java
@@ -1,17 +1,36 @@
 package ics4u.a8;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+
 /**
- * Assignment 7
- * Pg 262
- * Question 4
- *  You have been hired by a wicked witch with a strong interest in children.
- *  The program that she uses to help her maintain records has a class
- *  Child that contains the ﬁelds
- *  private int height; // height in cm private
- *  double mass; // mass in kg T
- *  he witch wants you to write an equals method for the class.
- *  The witch considers two Child objects to be equal if their heights diﬀer
- *  by no more than 2 cm and their masses diﬀer by no more than 0.5 kg.
+ * Assignment 8
+ * Question 11
+ * The Sieve of Eratosthenes is the name given to an algorithm for ﬁnding prime numbers
+ * (natural numbers having exactly two distinct natural number divisors, one and themselves).
+ * The sieve starts by considering all numbers greater than one as possible primes.
+ * <p>
+ * 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ...
+ * <p>
+ * The ﬁrst number in this list, 2, must be prime but all multiples of two cannot be prime
+ * and hence can be eliminated from the list to give us
+ * <p>
+ * 2 3 /4 5 /6 7 /8 9 1/0/ 11 1/2/ 13 1/4/ 15 ...
+ * <p>
+ * The next number still in the list, 3, must also be prime since it was not eliminated when
+ * we found multiples of two. We can, however, eliminate all multiples of three to obtain
+ * <p>
+ * 2 3 /4 5 /6 \ 7 /8 9 \ 1/0/ 11 1/2/ \\ 13 1/4/ 1 \5 \ ...
+ * <p>
+ * In the algorithm, this process continues (eliminating multiples of ﬁve, seven, and so on) until
+ * it is no longer possible to eliminate any multiples of values left in the table. The values that
+ * remain must all be primes. Write a program to implement Eratosthene’s Sieve. The program should
+ * ask the user for an upper bound and should then print all the prime numbers less than or equal
+ * to that upper bound. In your program, use a boolean array to maintain a record of the numbers
+ * that are possible primes. The output should have eight values per line with each value
+ * right-justiﬁed in a ﬁeld that is ten characters wide. You may ﬁnd the methods of the Out
+ * class shown in Appendix A useful for this.
  *
  * @author Yu Liu
  */
@@ -19,45 +38,126 @@ package ics4u.a8;
 public class A8Q11 {
 
     public static void main(String[] args) {
-        // create and test Child objects
-        Child child1 = new Child(120, 27.0);
-        Child child2 = new Child(122, 26.6);
-        System.out.println("child1: " + child1);
-        System.out.println("child2: " + child2);
-        if (child1.equals(child2)){
-            System.out.println("child1 equals to child2");
-        } else {
-            System.out.println("child1 not equal to child2");
+        // get the upper bound for the sieve
+        int n;
+        System.out.print("Enter the upper bound between [2, 10000]: ");
+        while ((n = _In.getInt()) >= 2 && n <= 10000) {
+            // calculate prime numbers
+            int[] primes = sieveOfEratosthenes(n);
+            // print them out in a table
+            int rows = primes.length / 8;
+            for (int i = 0; i < rows - 1; i++) {
+                for (int j = 0; j < 8; j++) {
+                    _Out.print(primes[i * 8 + j], 10);
+                }
+                System.out.println();
+            }
+            for (int j = 0; j < primes.length % 8; j++) {
+                _Out.print(primes[rows * 8 + j], 10);
+            }
+            System.out.println();
+            System.out.print("Enter the upper bound between [2, 10000]: ");
         }
     }
 
-    private static class Child{
-        private int height; // cm
-        double mass; // kg
-
-        Child(int height, double mass) {
-            this.height = height;
-            this.mass = mass;
+    /**
+     * Computes the set of all prime numbers between 2 and an upper bound (inclusive)
+     * with the Sieve of Eratosthenes algorithm
+     *
+     * @param N the integer upper bound of the sieve. Note that the JVM doesn't usually have
+     *          enough memory configured to store the entire sieve because this implementation
+     *          is not segmented. Upper bound must be greater or equal to 2
+     * @return an integer array containing all prime numbers between the interval of [2, n]
+     */
+    static int[] sieveOfEratosthenes(int N) {
+        // check bounds
+        if (N < 2) throw new IllegalArgumentException("The Sieve cannot have an upper bound less than 2");
+        // initialize array of candidate numbers
+        int[] C = new int[N - 1];
+        for (int i = 0; i < N - 1; i++) C[i] = i + 2;
+        // q represents the upper bound of indices that actually need to be checked
+        int q = (int) Math.sqrt(N);
+        // loop through numbers between [2, q] with the variable p
+        for (int p = 2; p <= q; p++) {
+            // skip if the number is already determined to be not prime
+            if (C[p - 2] == -1) continue;
+            // loop through multiples (excluding itself) of p and mark it as not prime
+            for (int i = p * 2; i < N + 1; i += p) C[i - 2] = -1;
         }
+        // Find the number of primes in the sieve
+        int k = 0;
+        for (int i = 0; i < N - 1; i++) if (C[i] > 0) k++;
+        // create an array with the proper size
+        int[] P = new int[k];
+        // copy over prime values
+        for (int i = 0, j = 0; i < N - 1; i++) if (C[i] > 0) P[j++] = C[i];
+        return P;
+    }
 
-        public boolean equals(Object obj) {
-            if (obj instanceof Child){
-                Child other = (Child) obj;
-                // check if height difference is less than 2 and mass difference is less than 0.5
-                return epsilonEquals(height, other.height, 2.0) && epsilonEquals(mass, other.mass, 0.5);
+    private static class _Out {
+
+        public static void print(int n, int fieldWidth) {
+            StringBuffer s = new StringBuffer(String.valueOf(n));
+            int stop = fieldWidth - s.length();
+            for (int i = 0; i < stop; i++) s.insert(0, " ");
+            System.out.print(s);
+        }
+    }
+
+
+    private static class _In {
+        private static InputStreamReader r = new InputStreamReader(System.in);
+        private static BufferedReader br = new BufferedReader(r);
+
+        // Read a String from standard system input
+        static String getString() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                return "";
             }
-            return false;
         }
 
-        /**
-         * Returns whether two numbers differ by no more than an epsilon number
-         */
-        private static boolean epsilonEquals(double a, double b, double epsilon){
-            return a - epsilon <= b &&  a + epsilon >= b;
+        // Read a Number as a String from standard system input
+        // Return the Number
+        private static Number getNumber() {
+            String numberString = getString();
+            try {
+                numberString = numberString.trim().toUpperCase();
+                return NumberFormat.getInstance().parse(numberString);
+            } catch (Exception e) {
+                // If any exception occurs, just return zero
+                return new Integer(0);
+            }
         }
 
-        public String toString() {
-            return "Child(height=" + height + ", mass=" + mass + ")";
+        // Read an int from standard system input
+        static int getInt() {
+            return getNumber().intValue();
+        }
+
+        // Read a long from standard system input
+        static long getLong() {
+            return getNumber().longValue();
+        }
+
+        // Read a float from standard system input
+        static float getFloat() {
+            return getNumber().floatValue();
+        }
+
+        // Read a double from standard system input
+        static double getDouble() {
+            return getNumber().doubleValue();
+        }
+
+        // Read a char from standard system input
+        static char getChar() {
+            String s = getString();
+            if (s.length() >= 1)
+                return s.charAt(0);
+            else
+                return '\n';
         }
     }
 }
--- src/main/java/ics4u/a8/A8Q3.java
+++ src/main/java/ics4u/a8/A8Q3.java
@@ -3,16 +3,24 @@ package ics4u.a8;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.NumberFormat;
+import java.util.Arrays;
 
 /**
- * Assignment 7
- * Pg 274
+ * Assignment 8
  * Question 3
- * <p>
- * Write a program that has a main method that uses the Fraction class to solve the following
- * problem: prompt the user for three fractions, a, b, and c, compute the value of ab−c2, print
- * the result, and print the sum of the squares of the numerator and denominator of the result.
- * <p>
+ * . Assuming that a Fraction class has been deﬁned as shown, write a fragment
+ * that could be used to create an array of 100 Fraction objects representing
+ * the fractions
+ * 0 1
+ * ,
+ * 1 2
+ * ,
+ * 2 3
+ * ,...,
+ * 99 100
+ * class Fraction { private int num; private int den;
+ * public Fraction (int n, int d) { num = n; den = d; }
+ * }
  * ICS4U class 2019
  *
  * @author Yu Liu
@@ -21,30 +29,13 @@ import java.text.NumberFormat;
 public class A8Q3 {
 
     public static void main(String[] args) {
-        Fraction a = getUserInputFraction("a");
-        Fraction b = getUserInputFraction("b");
-        Fraction c = getUserInputFraction("c");
-        // calculate the result of a * n - c^2
-        Fraction result = a.times(b).plus(c.times(c).negate()).reduced();
-        System.out.println("a * b - c^2 = " + result);
-        // calculate sum of squares of numerator and denominator
-        System.out.println("sum of squares = " + result.num * result.num + result.den * result.den);
-    }
-
-    /**
-     * Get the user input of a Fraction
-     */
-    private static Fraction getUserInputFraction(String name) {
-        System.out.print("Enter the numerator for fraction " + name + ": ");
-        int num = _In.getInt();
-        System.out.print("Enter the denominator for fraction " + name + ": ");
-        int den = _In.getInt();
-        while (den == 0) {
-            System.out.println("The denominator cannot be zero");
-            System.out.print("Enter the denominator for fraction " + name + ": ");
-            den = _In.getInt();
+        Fraction[] fractions = new Fraction[100];
+        for (int i = 0; i <fractions.length; i++) {
+            // create the fraction at the index
+            fractions[i] = new Fraction(i, i + 1);
+            // print out the fraction
+            System.out.println(fractions[i]);
         }
-        return new Fraction(num, den);
     }
 
     /**
@@ -62,115 +53,10 @@ public class A8Q3 {
         }
 
         /**
-         * Check if the Fraction is equal to another in value
-         */
-        public boolean equals(Object obj) {
-            if (obj instanceof Fraction) {
-                Fraction other = (Fraction) obj;
-                return num / den == other.num / other.den;
-            }
-            return false;
-        }
-
-
-        /**
-         * Reduces the fraction to lowest terms
-         */
-        Fraction reduced() {
-            int gcd = gcd(num, den);
-            return gcd > 1 ? new Fraction(num / gcd, den / gcd) : new Fraction(num, den);
-        }
-
-        /**
-         * Uses the Euclidean algorithm to calculate the greatest common divisor
-         */
-        private static int gcd(int a, int b) {
-            return b == 0 ? a : gcd(b, a % b);
-        }
-
-        /**
-         * Returns the sum of this fraction and another fraction
-         */
-        Fraction plus(Fraction other) {
-            return new Fraction(num * other.den + other.num * den, den * other.den);
-        }
-
-        /**
-         * Returns the inverse (reciprocal) of this fraction
-         */
-        private Fraction negate() {
-            return new Fraction(-num, den);
-        }
-
-        /**
-         * Returns the product of this fraction and another fraction
-         */
-        private Fraction times(Fraction other) {
-            return new Fraction(num * other.num, den * other.den);
-        }
-
-        /**
          * Converts the fraction to string (for printing)
          */
         public String toString() {
             return "Fraction(num = " + num + ", den = " + den + ")";
-        }
-    }
-
-
-    private static class _In {
-        private static InputStreamReader r = new InputStreamReader(System.in);
-        private static BufferedReader br = new BufferedReader(r);
-
-        // Read a String from standard system input
-        static String getString() {
-            try {
-                return br.readLine();
-            } catch (Exception e) {
-                return "";
-            }
-        }
-
-        // Read a Number as a String from standard system input
-        // Return the Number
-        private static Number getNumber() {
-            String numberString = getString();
-            try {
-                numberString = numberString.trim().toUpperCase();
-                return NumberFormat.getInstance().parse(numberString);
-            } catch (Exception e) {
-                // If any exception occurs, just return zero
-                return new Integer(0);
-            }
-        }
-
-        // Read an int from standard system input
-        static int getInt() {
-            return getNumber().intValue();
-        }
-
-        // Read a long from standard system input
-        static long getLong() {
-            return getNumber().longValue();
-        }
-
-        // Read a float from standard system input
-        static float getFloat() {
-            return getNumber().floatValue();
-        }
-
-        // Read a double from standard system input
-        static double getDouble() {
-            return getNumber().doubleValue();
-        }
-
-        // Read a char from standard system input
-        static char getChar() {
-            String s = getString();
-            if (s.length() >= 1)
-                return s.charAt(0);
-            else
-                return '\n';
         }
     }
 }
