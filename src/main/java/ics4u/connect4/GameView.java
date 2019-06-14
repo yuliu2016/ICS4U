70a5285c4d1421233f8334b9efced24d2dd45f7b
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-06-13 21:33:52 -0400

    implement mouse listener

--- src/main/java/ics4u/connect4/GameView.java
+++ src/main/java/ics4u/connect4/GameView.java
@@ -8,49 +8,75 @@ import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.RoundRectangle2D;
 
+/**
+ * @noinspection FieldCanBeLocal
+ */
 public class GameView extends JComponent {
 
     private static Color darkCyan = new Color(0, 125, 125);
     private static Color lightGray = new Color(224, 224, 224);
-    private Font mainFont;
 
+    private int lastWidth = 0;
+    private int lastHeight = 0;
+
+    private double scale = 0.0;
+    private double x = 0.0;
+    private double y = 0.0;
+    private double width = 0.0;
+    private double height = 0.0;
+
+    private int hoverColumn = -1;
+
+    private String message = "Connect 4";
+
+    private Font mainFont;
     private Connect4 state;
 
-    public GameView(Connect4 state) {
+    private MouseListener mouseListener = new MouseListener() {
+        public void mouseClicked(MouseEvent e) {
 
+        }
+
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+    };
+
+    private MouseMotionListener mouseMotionListener = new MouseMotionListener() {
+        public void mouseDragged(MouseEvent e) {
+        }
+
+        public void mouseMoved(MouseEvent e) {
+            int newCol = -1;
+            if (scale != 0.0) {
+                int mouseX = e.getX();
+                int cols = state.getColumns();
+                double min = x + 2 * scale;
+                double max = min + cols * 12 * scale;
+                if (mouseX >= min && mouseX <= max) {
+                    newCol = (int) ((mouseX - min) / (12 * scale));
+                }
+            }
+            if (newCol != hoverColumn) {
+                hoverColumn = newCol;
+                repaint();
+            }
+        }
+    };
+
+    public GameView(Connect4 state) {
         this.state = state;
         checkFonts();
-
-        addMouseListener(new MouseListener() {
-            public void mouseClicked(MouseEvent e) {
-
-            }
-
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        addMouseMotionListener(new MouseMotionListener() {
-            public void mouseDragged(MouseEvent e) {
-
-            }
-
-            public void mouseMoved(MouseEvent e) {
-            }
-        });
+        addMouseListener(mouseListener);
+        addMouseMotionListener(mouseMotionListener);
     }
 
     private void checkFonts() {
@@ -65,18 +91,10 @@ public class GameView extends JComponent {
         mainFont = new Font("Arial", Font.PLAIN, 11);
     }
 
-    int lastWidth = 0;
-    int lastHeight = 0;
-
-    double scale = 0.0;
-    double x = 0.0;
-    double y = 0.0;
-    double width = 0.0;
-    double height = 0.0;
-    String message = "Connect 4";
-
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+
+        // Set rendering parameters
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -140,7 +158,7 @@ public class GameView extends JComponent {
         g.setColor(darkCyan);
         g2.fill(bg2);
 
-        double diam = scale * 10;
+
         int[][] board = state.getBoard();
 
         board[1][1] = 2;
@@ -149,10 +167,23 @@ public class GameView extends JComponent {
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
                 Ellipse2D ellipse = new Ellipse2D.Double();
-                ellipse.setFrame(x + scale * (12 * i + 3), y + scale * (12 * j + 12 + 3), diam, diam);
-                int piece = board[j][i];
+                ellipse.setFrame(x + scale * (12 * i + 3), y + scale * (12 * j + 12 + 3),
+                        scale * 10, scale * 10);
+                int piece = board[rows - 1 - j][i];
                 g.setColor(piece == 0 ? Color.WHITE : (piece == 1 ? Color.RED : Color.YELLOW));
                 g2.fill(ellipse);
+            }
+        }
+
+        if (hoverColumn != -1) {
+            int row = rows - 1;
+            if (board[row][hoverColumn] == 0) {
+                while (row > 0 && board[row - 1][hoverColumn] == 0) row--;
+                g.setColor(Color.BLUE);
+                Ellipse2D hovierIndicator = new Ellipse2D.Double();
+                hovierIndicator.setFrame(x + scale * (12 * hoverColumn + 3 + 3.5),
+                        y + scale * (12 * (rows - 1 - row) + 12 + 3 + 3.5), scale * 3, scale * 3);
+                g2.fill(hovierIndicator);
             }
         }
 
